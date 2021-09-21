d27147ae221a46dcc9f0e6d3687446fc018b1371
AfrithMN <73326264+AfrithAhamedkey@users.noreply.github.com>
2021-09-21 10:48:22 +0530

    integrate_outputs

---
+++ Makefile
@@ -0,0 +1,37 @@
+    OBJECTS = loader.o kmain.o drivers/io/io.o memory/segmentation/memory_segments.o memory/segmentation/gdt.o drivers/interrupts/interrupts.o drivers/interrupts/interrupt_handlers.o drivers/interrupts/pic.o drivers/interrupts/keyboard.o drivers/interrupts/idt.o memory/heap/kheap.o memory/paging/paging.o
+    CC = gcc
+    CFLAGS = -m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector \
+             -nostartfiles -nodefaultlibs -Wall -Wextra -Werror -c
+    LDFLAGS = -T link.ld -melf_i386
+    AS = nasm
+    ASFLAGS = -f elf
+
+    all: kernel.elf
+
+    kernel.elf: $(OBJECTS)
+	ld $(LDFLAGS) $(OBJECTS) -o kernel.elf
+
+    os.iso: kernel.elf
+	cp kernel.elf iso/boot/kernel.elf
+	genisoimage -R                              \
+                    -b boot/grub/stage2_eltorito    \
+                    -no-emul-boot                   \
+                    -boot-load-size 4               \
+                    -A os                           \
+                    -input-charset utf8             \
+                    -quiet                          \
+                    -boot-info-table                \
+                    -o os.iso                       \
+                    iso
+
+    run: os.iso
+	bochs -f bochsrc.txt -q
+
+    %.o: %.c
+	$(CC) $(CFLAGS)  $< -o $@
+
+    %.o: %.s
+	$(AS) $(ASFLAGS) $< -o $@
+
+    clean:
+	rm -rf *.o kernel.elf os.iso
--- loader.s
+++ loader.s
@@ -1,17 +1,51 @@
-global loader                   ; the entry symbol for ELF
+    	global loader                   ; the entry symbol for ELF
+    
+    	KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
+    
+     	;setting up the Multiboot header - see GRUB docs for details			
+    	; align loaded modules on page boundaries
+	MODULEALIGN equ 1 << 0
 
-    MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-    FLAGS        equ 0x0            ; multiboot flags
-    CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
-                                    ; (magic number + checksum + flags should equal 0)
+	; provide memory map
+	MEMINFO equ 1 << 1
 
-    section .text:                  ; start of the text (code) section
+	; this is the Multiboot 'flag' field
+	FLAGS equ MODULEALIGN | MEMINFO
+
+	; 'magic number' lets bootloader find the header
+	MAGIC_NUMBER equ 0x1BADB002
+
+	; calculate the checksum (all options + checksum should equal 0)
+    	CHECKSUM equ - (MAGIC_NUMBER + FLAGS)
+	
+	; Kernel Start Physical Address is exported from linker script
+	extern KERNEL_PHYSICAL_START
+
+	; Kernel End Physical Address is exported from linker script
+	extern KERNEL_PHYSICAL_END
+
+	; start of the bss section
+    section .bss
+    align 4                         ; align at 4 bytes
+    KERNEL_STACK:                   ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
+    
+        
+    section .text                   ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
         dd MAGIC_NUMBER             ; write the magic number to the machine code,
-        dd FLAGS                    ; the flags,
-        dd CHECKSUM                 ; and the checksum
-
-    loader:                         ; the loader label (defined as entry point in linker script)
-        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+        dd FLAGS            ; write the align modules instruction
+        dd CHECKSUM                 ; write the checksum
+    
+    	; the loader label (defined as entry point in linker script)
+    loader:
+    	mov esp, KERNEL_STACK + KERNEL_STACK_SIZE  ; point esp to the start of the stack (end of memory area)
+    	
+    	extern kmain			; kmain function is defined elsewhere
+    	
+    	push $KERNEL_PHYSICAL_END	; the kernel end address
+    	push $KERNEL_PHYSICAL_START 	; and the kernel start address
+    	call kmain			; call kernel main function.
     .loop:
-        jmp .loop                   ; loop forever
+    	; loop forever
+      jmp .loop
